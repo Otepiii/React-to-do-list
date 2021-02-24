53dcde2fe02fcf9d2b9d60826dfdf5a6e7d04078
joseph Evangelista <oootep123@gmail.com>
2021-02-24 10:03:46 -0800

    cleaned up the header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
-import About from "./components/pages/About";
 import "./App.css";
 import axios from "axios";
 
@@ -69,7 +68,6 @@ class App extends Component {
                 </React.Fragment>
               )}
             />
-            <Route path="/about" component={About} />
           </div>
         </div>
       </Router>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,13 +5,6 @@ function Header() {
   return (
     <header style={headerStyle}>
       <h1>TodoList</h1>
-      <Link style={linkStyle} to="/">
-        Home
-      </Link>
-      |
-      <Link style={linkStyle} to="/about">
-        About
-      </Link>
     </header>
   );
 }
